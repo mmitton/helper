7f91f416a4694edd7c0f9c8fd4a9522470cffa34
Michael Conrad <mmitton@gmail.com>
2024-11-20 13:35:05 -0500

    Points

--- src/tile_set.rs
+++ src/tile_set.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{cmp::Ordering, fmt::Display};
 
 use crate::Integer;
 
@@ -143,19 +143,63 @@ impl<T: Integer> Tile<T> {
     }
 }
 
-#[derive(Copy, Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Point<T: Integer> {
+#[derive(Copy, Clone, Debug, Default, Hash, PartialEq, Eq)]
+pub struct Point<T: Integer, const INVERT_SORT: bool = false, const REVERSE_SORT: bool = false> {
     pub x: T,
     pub y: T,
 }
 
-impl<T: Integer> Display for Point<T> {
+impl<T: Integer, const INVERT_SORT: bool, const REVERSE_SORT: bool> Ord
+    for Point<T, INVERT_SORT, REVERSE_SORT>
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let cmp = if !INVERT_SORT {
+            // Sort X then Y
+            if self.x != other.x {
+                self.x.cmp(&other.x)
+            } else {
+                self.y.cmp(&other.y)
+            }
+        } else {
+            // Sort Y then X
+            if self.y != other.y {
+                self.y.cmp(&other.y)
+            } else {
+                self.x.cmp(&other.x)
+            }
+        };
+
+        if REVERSE_SORT {
+            match cmp {
+                Ordering::Less => Ordering::Greater,
+                Ordering::Equal => Ordering::Equal,
+                Ordering::Greater => Ordering::Less,
+            }
+        } else {
+            cmp
+        }
+    }
+}
+
+impl<T: Integer, const INVERT_SORT: bool, const REVERSE_SORT: bool> PartialOrd
+    for Point<T, INVERT_SORT, REVERSE_SORT>
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: Integer, const INVERT_SORT: bool, const REVERSE_SORT: bool> Display
+    for Point<T, INVERT_SORT, REVERSE_SORT>
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}x{}", self.x, self.y)
     }
 }
 
-impl<T: Integer> Point<T> {
+impl<T: Integer, const INVERT_SORT: bool, const REVERSE_SORT: bool>
+    Point<T, INVERT_SORT, REVERSE_SORT>
+{
     pub fn new(x: T, y: T) -> Self {
         Self { x, y }
     }
